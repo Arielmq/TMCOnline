4a3f6e16cd73cfd55834e0b728314b8d6c66c30b
ivan <ivansangomez6@gmail.com>
2025-05-24 18:49:00 -0500

    add json

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // 🔥 importante
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // 🔧 esto soluciona el problema
+    },
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
-  // 👇 Esto ayuda para desarrollo, pero en Vercel también puede ayudar
   server: {
-    historyApiFallback: true
-  }
+    historyApiFallback: true,
+  },
 })
